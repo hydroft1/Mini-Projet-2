ab95d4e1b6d24e9143e499dfb06f000ed584d7f5
hydroft1 <marquetalexandre@proton.me>
2023-12-21 09:51:32 +0100

    commit

--- before.py
+++ before.py
@@ -1,46 +1,45 @@
 import random
 import string
-import customtkinter as ctk
-from customtkinter import CTk, CTkLabel, CTkButton, CTkComboBox
+import tkinter as tk
+from tkinter import ttk
 from ttkthemes import ThemedTk
-import tkinter as ttk
 
-# function to generate password
-def generate_password(*args):
+# function to generate password 
+def generate_password():
     characters = string.ascii_letters + string.digits + string.punctuation
     password = "".join(random.choice(characters) for i in range(var.get()))
     output.config(text = password)
     output.config(text = password, font = ("Ubuntu", 20), justify = 'center')
-
+    
 # function to copy the password to clipboard
 def copy_to_clipboard():
-    app.clipboard_clear()
-    app.clipboard_append(output['text']) # Use cget() to get the text
+    root.clipboard_clear()
+    root.clipboard_append(output['text'])
 
-# create themed customtkinter window
-app = CTk()
-app.title("Password Generator")
-app.geometry("960x540")
+# create themed tkinter window
+root = ThemedTk(theme="equilux")
+root.title("Password Generator")
+root.geometry("960x540")
 
 # variable to hold the number of characters in the password
-var = ctk.IntVar()
+var = tk.IntVar()
 var.set(8)
 
 # create a dropdown menu for the number of characters
-dropdown = ttk.Combobox(app, textvariable= var, values= [8,9,10,11,12,13,14,15,16,17,18,19,20])
+dropdown = ttk.Combobox(root, textvariable= var, values= [8,9,10,11,12,13,14,15,16,17,18,19,20])
 dropdown.pack(pady = 5)
 
 # create 'generate' button
-generate_button = ttk.Button(app, text="Generate", command= generate_password)
+generate_button = ttk.Button(root, text="Generate", command= generate_password)
 generate_button.pack(pady = 5)
 
 # create 'copy' button
-copy_button = ttk.Button(app, text="Copy", command= copy_to_clipboard)
+copy_button = ttk.Button(root, text="Copy", command= copy_to_clipboard)
 copy_button.pack(pady = 5)
 
 # create output field
-output = ttk.Label(app)
+output = ttk.Label(root)
 output.pack(pady = 20)
 
 # run the main loop
-app.mainloop()
+root.mainloop()
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+dropdown = ttk.Combobox(root, textvariable= var, values= [8,9,10,11,12,13,14,15,16,17,18,19,20])
+dropdown.pack(pady = 5)
